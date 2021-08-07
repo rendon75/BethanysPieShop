52bd1d50595d177e13688bcfb8c8967c8d3c31d6
Rene Rendon Jr <rene.rendon@hotmail.com>
2021-08-07 08:22:18 -0500

    After Chapter 7, Improving the Views in the app.

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,10 @@ namespace BethanysPieShop
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+
             services.AddControllersWithViews();
         }
 
@@ -45,6 +49,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
